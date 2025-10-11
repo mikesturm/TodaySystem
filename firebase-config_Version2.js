49d2b0f9e78fa47a44da01c0d39cef1439c08352
Mike Sturm <mike.p.sturm@gmail.com>
2025-10-11 09:11:12 -0500

    Upload of Firebase link

---
+++ firebase-config_Version2.js
@@ -0,0 +1,13 @@
+// Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCOT-N5aW7idZGnl4tqd9SosO0NET_O4Ns",
+  authDomain: "the-today-system.firebaseapp.com",
+  projectId: "the-today-system",
+  storageBucket: "the-today-system.firebasestorage.app",
+  messagingSenderId: "5726180443",
+  appId: "1:5726180443:web:0b591811c0c9620e685b7e",
+  measurementId: "G-REDV143S80"
+};
+
+// Make it globally available
+window.__firebase_config = JSON.stringify(firebaseConfig);
